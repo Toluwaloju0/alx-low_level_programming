0f13ea51c37ba4875c5235ea319ae7bd80174249
Toluwaloju0 <teecoded1990@gmail.com>
2023-08-17 14:38:47 +0100

    alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+*print_alphabet_x10 - print alphabets 10 times
+*Return: void
+*/
+void print_alphabet_x10(void)
+{
+	int b = 0;
+	int c;
+
+	while (b < 10)
+	{
+		int c = 97;
+
+		while (c < 123)
+		{
+			putchar(c);
+			c++;
+		}
+
+		putchar('\n');
+		b++;
+	}
+}
+
+/**
+*main - check the code
+*Return: 0
+*/
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
