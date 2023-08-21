b0257cc23c2a8e15705582a075fef614fb73539c
Toluwaloju0 <teecoded1990@gmail.com>
2023-08-21 20:18:01 +0100

    functions

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _strlen - to print the length of a string
+* @s: a character
+* Return: integer
+*/
+int _strlen(char *s)
+{
+	int i;
+	{
+		while (*s = '\0')
+		i++
+	};
+	return (i);
+}
