82184c58171ba97ddc48372cec668c31701e7f6e
Toluwaloju0 <teecoded1990@gmail.com>
2023-08-21 22:55:01 +0100

    functions

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,15 +1,36 @@
 #include "main.h"
 #include <string.h>
 /**
-* rev_string - to reverse a string
+* length - to return the length of the string
 * @s: a string to be reversed
+* Return: int
+*/
+int length(char *s)
+{
+	int i = 0;
+
+	while (i != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+* rev_string - to reverse a string
+* @s: a character
 */
 void rev_string(char *s)
 {
-	int i = strlen(s) - 1;
+	int start = 0;
+	int end = i;
 
-	while (i >= 0)
+	while (start < end)
 	{
-		--i;
+		char temp = s[start];
+
+		s[start] = s[end];
+		s[end] = temp;
+		start++;
+		end--;
 	}
 }
