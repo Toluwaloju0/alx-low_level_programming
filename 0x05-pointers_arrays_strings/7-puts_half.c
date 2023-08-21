8f7d10b72788c5c0dcc12a9c320b848e11b8f1c4
Toluwaloju0 <teecoded1990@gmail.com>
2023-08-21 23:53:06 +0100

    functions

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* puts_half - to input the other half of a string
+* @str: string given
+*/
+void puts_half(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 1)
+		{
+			printf("%c", str[i]);
+		}
+		i++;
+	}
+	printf("\n");
+}
