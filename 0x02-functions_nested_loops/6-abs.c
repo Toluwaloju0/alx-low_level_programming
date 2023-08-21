057d7b48b5ab8a4a9a4b51576bd5b7702e636a3e
Toluwaloju0 <teecoded1990@gmail.com>
2023-08-21 14:56:07 +0100

    functions

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _abs - to print absolute value
+* @int: am integer
+* Return: int
+*/
+int _abs(int)
+{
+	int a;
+
+	if (a >= 0)
+	{
+		return (a);
+	}
+	else
+	{
+		return (a * -1);
+	}
+	_putchar('\n');
+}
