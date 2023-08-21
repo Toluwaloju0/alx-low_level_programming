0c68b4f2e36d026eaa787bd1be137b7f098a2e7f
Toluwaloju0 <teecoded1990@gmail.com>
2023-08-21 23:27:21 +0100

    functions

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* puts2 - to prints the first character and jump the next
+* @str: the string given
+*/
+void puts2(char *str)
+{
+	int i = char s;
+
+	while (int i % 2 = 0)
+	{
+		putchar(i);
+		i++
+	}
+	putchar('\n')
+}
