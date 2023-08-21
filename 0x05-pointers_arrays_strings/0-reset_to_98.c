99446b1559efc24cb326b3036f49b84725572062
Toluwaloju0 <teecoded1990@gmail.com>
2023-08-21 16:39:13 +0100

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* reset_to_98 - tom add 98 to the variable in *n
+* @*n: a pointer to n
+* *@n: an integer
+* Return: int *n
+*/
+void reset_to_98(int *n);
+{
+	int a;
+
+	n = &a;
+	*n 98;
+	return (0);
+}
