a2af1d659e679f6723909f26069334cae61221e2
Toluwaloju0 <teecoded1990@gmail.com>
2023-08-22 22:57:20 +0100

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,11 @@
-
+#include <stdio.h>
+/**
+* printf - to print without using printf and puts
+* @c: character to be printed
+* Return: 1
+*/
+extern int printf (const char *c)
+{
+	("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
