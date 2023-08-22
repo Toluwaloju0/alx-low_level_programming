26b50598dc8ff5a16e69598711df42cfab85cce8
Toluwaloju0 <teecoded1990@gmail.com>
2023-08-22 23:23:40 +0100

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
-* printf - to print without using printf and puts
-* @c: character to be printed
+* main - to print without using printf and puts
 * Return: 1
 */
-int main(void) 
+int main(void)
 {
-	FILE *file = stderr;
-	fprintf(file, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	FILE *f = stderr;
+
+	fprintf(f, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
