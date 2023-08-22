3129d9e50a989e4605860458a6cee591323591b3
Toluwaloju0 <teecoded1990@gmail.com>
2023-08-22 11:01:50 +0100

    functions

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_array - to print the members of an array
+* @a: a pointer to integer
+* @n: an integer
+*/
+void print_array(int *a, int n)
+{
+	int array[n];
+
+	while (n >= 0)
+	{
+		printf("%d, ", array[n]);
+		n++;
+	}
+	printf("\n")
+}
