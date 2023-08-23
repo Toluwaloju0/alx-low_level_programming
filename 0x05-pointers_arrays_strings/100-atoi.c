1adbaa6e6ee05cbdff3bfd31e6a490bf241bef2e
Toluwaloju0 <teecoded1990@gmail.com>
2023-08-23 11:18:07 +0100

    strings

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* _atoi - to change strings to integers
+* @s: a character with strings
+* Return: integer
+*/
+int _atoi(char *s)
+{
+	
---
+++ 0x06-pointers_arrays_strings/1-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+/**
+* *_strcat - to concacate two strings toghether
+* @dest: the destination string
+* @src: the string to be concacated
+* Return: new string dest
+*/
+char *_strcat(char *dest, char *src)
+{
+	char *d = dest;
+
+	while (*d != '\0')
+	{
+		d++;
+	}
+	while (*src != '\n')
+	{
+		*d = *src;
+		*d++;
+		*src++;
+	}
+	*d = '\0';
+	return (dest);
+}
