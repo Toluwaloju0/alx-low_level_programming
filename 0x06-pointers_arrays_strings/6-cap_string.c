85813635c53ef3611a310dc36be00033b8539d24
Toluwaloju0 <teecoded1990@gmail.com>
2023-08-24 19:49:50 +0100

    strings

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <ctype.h>
+/**
+* *cap_string - to capitalize the words of a string
+* @a: the string to be edited
+* Return: the edited character
+*/
+char *cap_string(char *a)
+{
+	int b = 0
+
+	if (b = 9 || b >= 32 && b <= 47 || b >= 58 && b <= 64 ||
