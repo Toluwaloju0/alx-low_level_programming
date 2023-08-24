f8dcca343e5b1f641ab521f9f9c9dae23abcacf6
Toluwaloju0 <teecoded1990@gmail.com>
2023-08-24 20:52:54 +0100

    more functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _isupper - to check if a character is uppercase
+* @c: the character
+* Return: 1 if upper and 0 if lower
+*/
+int _isupper(int c)
+{
+	if (c >= 65 || c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* mul - to multiply two integers
+* @a: first integer
+* @b: second integer
+* Return: the multiplied integer
+*/
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
