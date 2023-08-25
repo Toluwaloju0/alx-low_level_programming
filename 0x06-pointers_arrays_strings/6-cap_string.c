b27eaab10bdec36b0cad5cbff4f1ae1cad200ce3
Toluwaloju0 <teecoded1990@gmail.com>
2023-08-25 21:20:54 +0100

    arrays

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,6 +7,59 @@
 */
 char *cap_string(char *a)
 {
-	int b = 0
+	int b = 0;
 
-	if (b = 9 || b >= 32 && b <= 47 || b >= 58 && b <= 64 ||
+	if (b = 32)
+	{
+		a[b + 1] = toupper(a[b + 1]);
+	}
+	else if (b = 9)
+	{
+		a[b + 1] = toupper(a[b + 1]);
+	}
+	else if (b = 13)
+	{
+		a[b + 1] = toupper(a[b + 1]);
+	}
+	else if (b = 44)
+	{
+		a[b + 1] = toupper(a[b + 1]);
+	}
+	else if (b = 59)
+	{
+		a[b + 1] = toupper(a[b + 1]);
+	}
+	else if ( b = 46)
+	{
+		a[b + 1] = toupper(a[b + 1]);
+	}
+	else if (b = 33)
+	{
+		a[b + 1] = toupper(a[b + 1]);
+	}
+	else if (b = 63)
+	{
+		a[b + 1] = toupper(a[b + 1]);
+	}
+	else if (b = 34)
+	{
+		a[b + 1] = toupper(a[b + 1]);
+	}
+	else if (b = 40)
+	{
+		a[b + 1] = toupper(a[b + 1]);
+	}
+	else if (b = 41)
+	{
+		a[b + 1] = toupper(a[b + 1]);
+	}
+	else if (b = 123)
+	{
+		a[b + 1] = toupper(a[b + 1]);
+	}
+	else if (b = 125)
+	{
+		a[b + 1] = toupper(a[b + 1]);
+	}
+	return (a);
+}
