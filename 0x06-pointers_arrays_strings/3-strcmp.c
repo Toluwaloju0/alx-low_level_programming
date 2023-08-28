ba090d26229e91258a3e61f4af1c0725e21688f4
Toluwaloju0 <teecoded1990@gmail.com>
2023-08-28 07:15:46 +0100

    strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,22 +8,36 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 != '\0' && *s2 != '\0')
+	int a = strlen(s1);
+	int b = strlen(s2);
+
+	if (a > b)
 	{
-		if (*s1 > *s2)
-		{
-			return (15);
-		}
-		else if (*s1 < *s2)
-		{
-			return (-15);
-		}
-		else
-		{
-			return (0);
-		}
-		s1++;
-		s2++;
+		return (15);
 	}
-	return (0);
+	else if (a < b)
+	{
+		return (-15);
+	}
+	else
+	{
+		while (*s1 != '\0' && *s2 != '\0')
+		{
+			if (*s1 > *s2)
+			{
+				return (15);
+			}
+			else if (*s1 < *s2)
+			{
+				return (-15);
+			}
+			else
+			{
+				return (0);
+			}
+			s1++;
+			s2++;
+		}
+		return (0);
+	}
 }
