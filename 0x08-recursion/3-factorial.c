b6ccdcd3edf41de7472e896050751763f247846f
Toluwaloju0 <teecoded1990@gmail.com>
2023-08-30 21:56:22 +0100

    recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,5 +1,9 @@
 #include "main.h"
-
+/**
+* factorial - to print the factorial of an integer
+* @n: the number given to factorial
+* Return: the factorised number
+*/
 int factorial(int n)
 {
 	if (n < 0)
