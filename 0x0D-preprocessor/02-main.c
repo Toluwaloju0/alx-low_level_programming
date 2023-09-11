2a0f700fcde6bbd1d686f807ad32f49daed11d09
Toluwaloju0 <teecoded1990@gmail.com>
2023-09-11 21:29:13 +0100

    0x0D. C - Preprocessor

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - to print the name a file is compiled from
+* Return: 0
+*/
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,3 @@
+#include <stdio.h>
+#include "main.h"
+#define SUM(x, y) ((x) + (y))
