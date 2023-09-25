1c736de5d884e2d7bf3e8400ecf9ae619dd96b38
Toluwaloju0 <teecoded1990@gmail.com>
2023-09-25 11:35:11 +0100

    deffering

--- 0x0A-argc_argv/main.h
+++
@@ -1,4 +0,0 @@
-int _putchar(char c);
-int main(int argc, char* argv[]);
-int main(int argc, __attribute__ ((unused))char*argv[]);
-
--- 0x0F-function_pointers/function_pointers.h
+++
@@ -1,9 +0,0 @@
-#include <stddef.h>
-#ifndef FUNCTION_POINTER_H
-#define FUNCTION_POINTER_H
-
-int _putchar(char c);
-void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-
-#endif
