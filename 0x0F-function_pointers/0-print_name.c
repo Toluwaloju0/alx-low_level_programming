f3faf3c0a01bf6d2d2509bbce79777cc28288809
Toluwaloju0 <teecoded1990@gmail.com>
2023-10-11 22:21:09 +0100

    function pointer

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stddef.h>
 /**
 * print_name - to print a name
 * @name: the name to be printed
@@ -8,5 +9,9 @@
 void print_name(char *name, void (*f)(char *))
 
 {
+	if (f == NULL || name == NULL)
+	{
+		return;
+	}
 	f(name);
 }
--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-#include <stdlib.h>
+#include <stddef.h>
 /**
 *array_iterator - to execute a function
 * @array: an array of integers
@@ -12,6 +12,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i = 0;
 
+	if (action == NULL || size <= 0)
+	{
+		return;
+	}
 	while (i < size)
 	{
 		action(array[i]);
