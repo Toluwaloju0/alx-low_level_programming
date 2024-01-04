42a324300673613e6aae1c5d5e8bd6192b518aca
Toluwaloju0 <teecoded1990@gmail.com>
2024-01-04 21:41:22 +0100

    doubly linked list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -29,7 +29,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->n = n;
 	while (t->next != NULL)
 	{
-		if (a == idx)
+		if (a == (idx - 1))
 		{
 			ch = t->prev;
 			ch->next = new;
