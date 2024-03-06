bd67fc5f08ae06d45fb98cd337d43ead1a98e706
Toluwaloju0 <teecoded1990@gmail.com>
2024-03-06 22:29:45 +0100

    opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - To convert the function to opcodes
+ * @argc: number of arguments passed to the program
+ * @argv: Vector to the arguments
+ * Return: None
+ */
+
+ void main(int argc, char **argv)
+ {
+ 	int a, b;
+	unsigned char *ptr = (unsigned char*)main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	a = atoi(argv[1]);
+
+	if (a < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (b = 0; b < a; b++)
+	{
+		printf("%02x ", ptr[b]);
+	}
+	printf("\n");
+}
