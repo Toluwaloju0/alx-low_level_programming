66eec08c0fef3280391d7fb88864c69ca8c4bccc
Toluwaloju0 <teecoded1990@gmail.com>
2024-03-26 22:42:53 +0100

    files

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -28,7 +28,7 @@ int main(int ac, char **av)
 	fd2 = open(av[2], O_RDWR | O_TRUNC | O_CREAT, 0664);
 	if (fd1 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file     %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 	if (fd2 == -1)
