47521a76278b26b98e31489b614c362ad833c27b
Toluwaloju0 <teecoded1999@gmail.com>
2024-03-26 03:21:25 +0100

    files

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,6 +23,11 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
 	write(fd, text_content, strlen(text_content));
 	close(fd);
 	return (1);
