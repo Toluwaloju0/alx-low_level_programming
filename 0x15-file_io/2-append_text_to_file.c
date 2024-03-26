e80de7cf711832c5cee9aab74665b881f596c1d2
Toluwaloju0 <teecoded1999@gmail.com>
2024-03-26 03:25:32 +0100

    files

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,6 +23,11 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
 	write(fd, text_content, strlen(text_content));
 	close(fd);
 	return (1);
