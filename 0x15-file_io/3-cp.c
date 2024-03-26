081e5be3fb1f7f7793200ac6110175daad1dc6b5
Toluwaloju0 <teecoded1999@gmail.com>
2024-03-26 16:59:50 +0100

    files

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,28 +14,37 @@
 int main(int ac, char **av)
 
 {
-	int fd1, fd2, wrn;
+	int fd1, fd2, wrn, a = 0;
+	off_t fl;
 	char *buf[BUFSIZ + 1];
 
 	if (ac != 3)
 	{
-		fprintf(stderr, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	fd1 = open(av[1], O_RDWR);
+	fd2 = open(av[2], O_RDWR | O_TRUNC | O_CREAT,
+0664);
 	if (fd1 == -1)
 	{
-		fprintf(stderr, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	wrn = read(fd1, buf, 1024);
-	fd2 = open(av[2], O_RDWR | O_TRUNC | O_CREAT, 0664);
 	if (fd2 == -1)
 	{
-		fprintf(stderr, "Error: Can't write to %s\n", av[2]);
-		exit(99);
+                dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);	
 	}
-	write(fd2, buf, wrn);
+
+	fl = lseek(fd1, 0, SEEK_END);
+	while (a < fl)
+	{
+		lseek(fd1, a, SEEK_SET);
+		wrn = read(fd1, buf, fl);
+		write(fd2, buf, wrn);
+		a += 1024;
+	}
 	close(fd1);
 	close(fd2);
 	return (0);
