684bea84e3a7f632a71b650a1d10fa66890082e8
Toluwaloju0 <teecoded1990@gmail.com>
2024-04-01 20:50:21 +0100

    hash tables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -60,7 +60,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 
-	index = hash_djb2((const unsigned char*)key);
+	index = hash_djb2((const unsigned char *)key);
 
 	index = index % ht->size;
 
