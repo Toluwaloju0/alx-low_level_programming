aacd63e2907973b197885a73023de046fa0e1084
Toluwaloju0 <teecoded1990@gmail.com>
2024-04-01 16:41:56 +0100

    hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,9 +12,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *h;
 
 
-	h = malloc(sizeof(hash_table_t) * size);
+	h = malloc(sizeof(hash_table_t));
 	if (h == NULL)
 	{
+		return (NULL);
+	}
+	h->size = size;
+	h->array = malloc(sizeof(hash_node_t) * size);
+	if (h->array == NULL)
+	{
+		free(h);
 		return (NULL);
 	}
 	return (h);
