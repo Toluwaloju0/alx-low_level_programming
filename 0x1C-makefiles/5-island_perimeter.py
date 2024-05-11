159bbdba4cccfd8fb27fa926e8dc30ca2a846f61
Toluwaloju0 <teecoded1990@gmail.com>
2024-05-11 14:37:23 +0100

    makefiles

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" A module to calculate the perimeter of an island"""
+
+
+def island_perimeter(grid):
+    """ A function to return the perimeter of a grid
+    Args:
+        grid: list of list of integers
+    """
+
+
