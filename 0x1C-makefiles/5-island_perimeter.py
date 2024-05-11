3b0c5d9adf9c014ec8baf97d810b107b5c193ebb
Toluwaloju0 <teecoded1990@gmail.com>
2024-05-11 16:43:26 +0100

    makkefiles

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,18 +8,30 @@ def island_perimeter(grid):
         grid: list of list of integers
     """
 
-    perimeter = 0
+    try:
+        perimeter = 0
 
-    for i in range(len(grid)):
-        for j in range(len(grid)):
-            if grid[i][j] == 1:
-                if i == 0 or grid[i - 1][j] == 0:
-                    perimeter += 1
-                if i == len(grid) - 1 or grid[i + 1][j] == 0:
-                    perimeter += 1
-                if j == 0 or grid[i][j - 1] == 0:
-                    perimeter += 1
-                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
-                    perimeter += 1
+        for a in grid:
+            for b in a:
+                if b == 1:
+                    continue
+                else:
+                    for i in range(len(grid)):
+                        for j in range(len(grid)):
+                            if grid[i][j] == 1:
+                                if i == 0 or grid[i - 1][j] == 0:
+                                    perimeter += 1
+                                if i == len(grid) - 1 or grid[i + 1][j] == 0:
+                                    perimeter += 1
+                                if j == 0 or grid[i][j - 1] == 0:
+                                    perimeter += 1
+                                if j == len(grid[i]) - 1:
+                                    perimeter += 1
+                                if grid[i][j + 1] == 0:
+                                    perimeter += 1
 
-    return perimeter
+                    return perimeter
+        return 2 * (len(grid) + len(grid[0]))
+
+    except Exception:
+        return 0
