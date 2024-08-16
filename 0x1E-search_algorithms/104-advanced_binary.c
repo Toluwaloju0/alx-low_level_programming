fc531efacf75c3c417f3cc6529ad03f1966cb6a0
Toluwaloju0 <teecoded1990@gmail.com>
2024-08-16 13:15:20 +0100

    search algoriithm

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -2,6 +2,31 @@
 #include "search_algos.h"
 
 /**
+ * print - To print an array
+ * @size: The size of the array
+ * @array: the array to be printed
+ */
+
+void print(size_t size, int *array);
+void print(size_t size, int *array)
+
+{
+	size_t a;
+
+	printf("Searching in array: ");
+	for (a = 0; a < size; a++)
+	{
+		if (a == size - 1)
+		{
+			printf("%d\n", array[a]);
+			break;
+		}
+		printf("%d, ", array[a]);
+	}
+}
+
+
+/**
  * advanced_binary - To searcg for a value using recursive binary search
  * @array: the array to search for
  * @size: the size of the array
@@ -9,43 +34,35 @@
  * Return: the index og the value searched for
  */
 
-int advanced_binary(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value)
 
 {
-	size_t min = 0, mid, max, tras;
+	size_t mid;
 	int *new, a;
 
 	if (array == NULL || size == 0)
 	{
 		return (-1);
 	}
-	max = size - 1;
-	printf("Searching in array: ");
-	for (tras = 0; tras <= max; tras++)
-	{
-		if (tras == max)
-		{
-			printf("%d\n", array[tras]);
-			break;
-		}
-		printf("%d, ", array[tras]);
-	}
-	mid = (min + max) / 2;
+
+	print(size, array);
+
+	mid = (size - 1) / 2;
+
 	if (array[mid] == value && array[mid - 1] != value)
 	{
-		return (mid);
+		return (size);
 	}
-	else if (array[mid] <  value)
+	else if (array[mid] < value)
 	{
 		new = &array[mid + 1];
-		size = max - mid;
+		size = size - mid - 1;
 		a = advanced_binary(new, size, value);
 		if (a == -1)
 		{
 			return (-1);
 		}
-		return (mid + 1 + a);
+		return (mid + a);
 	}
 	else if (array[mid] >= value)
 	{
@@ -55,7 +72,7 @@ int advanced_binary(int *array, size_t size, int value)
 		{
 			return (-1);
 		}
-		return (mid - a);
+		return (size - a);
 	}
 	return (-1);
 }
